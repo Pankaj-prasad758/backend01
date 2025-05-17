e61ec3d9dd71931eb69fdb3be6fcf9f9605e7e5f
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-05-17 18:30:16 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import dotenv from "dotenv"
+// import mongoose from "mongoose";
+// import express from "express";
+// import { DB_NAME } from "./constants.js";
+
 import connectDB from "./db/db.js";
 dotenv.config({
     path:"./.env"
