d56e5a4e33c6541be0de4bdaf5b1233e82bce772
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-05-17 18:26:10 +0530

    db connected

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv"
+import connectDB from "./db/db.js";
+dotenv.config({
+    path:"./.env"
+})
+
+connectDB()
+
+
+// const app = express();
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("server error", error);
+//       4;
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("Error in index.js in src", error);
+//   process.exit(1)
+    
+//   }
+// })();
